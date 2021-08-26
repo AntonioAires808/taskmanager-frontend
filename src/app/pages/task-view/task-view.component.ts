490e718c30f51acb00412c6f43ae40c2f96ec228
AntonioAires808 <tomex808@hotmail.com>
2021-08-26 21:02:46 +0100

    start connect to api

--- src/app/pages/task-view/task-view.component.ts
+++ src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { TaskService } from 'src/app/task.service';
 
 @Component({
   selector: 'app-task-view',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() { }
+  lists: any;
 
-  ngOnInit(): void {
+  constructor(private taskService: TaskService, private route: ActivatedRoute) {
+
+   }
+
+  ngOnInit() {
+    this.route.params.subscribe( (params: Params) => {
+
+    });
+
+    // this.taskService.getLists().subscribe( (lists: any[]) => {
+    //   this.lists = lists;
+    // });
   }
 
 }
